908edfb0bace7d60fb25fbbde36ac1311db59ff0
Julius Sozin <J.Batkovich@gmail.com>
2019-07-03 19:43:13 -0400

    back to top button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
 var lightbox = $('.work2 a').simpleLightbox();
 
-// mobile dropdown
+// Disabling the scroll at the top
+const bttBtn = document.querySelector('.back-to-top');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+bttBtn.style.display = 'none';
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    bttBtn.style.display = 'block';
+  } else {
+    bttBtn.style.display = 'none';
+  }
+}
+
+// smooth scroll for back to top
+$('.back-to-top').on('click', function() {
+  const top = $('#top').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: top
+    },
+    500
+  );
+});
